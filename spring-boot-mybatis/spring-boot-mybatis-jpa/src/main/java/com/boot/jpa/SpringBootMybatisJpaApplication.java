ab77b14ba87be39575bbd5224477f5120518275a
xp-zhao <13688396271@163.com>
2019-06-28 18:08:13 +0800

    SpringBoot使用JPA学习

---
+++ spring-boot-mybatis/spring-boot-mybatis-jpa/src/main/java/com/boot/jpa/SpringBootMybatisJpaApplication.java
@@ -0,0 +1,13 @@
+package com.boot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootMybatisJpaApplication.class, args);
+  }
+
+}
