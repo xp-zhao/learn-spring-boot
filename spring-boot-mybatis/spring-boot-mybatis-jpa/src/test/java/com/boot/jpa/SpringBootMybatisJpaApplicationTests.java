cceb4edae17bb21578ff6f4f36392b3570701a3c
xp-zhao] <13688396271@163.com>
2019-06-29 22:56:29 +0800

    操作数据库

--- spring-boot-mybatis/spring-boot-mybatis-jpa/src/test/java/com/boot/jpa/SpringBootMybatisJpaApplicationTests.java
+++ spring-boot-mybatis/spring-boot-mybatis-jpa/src/test/java/com/boot/jpa/SpringBootMybatisJpaApplicationTests.java
@@ -1,7 +1,9 @@
 package com.boot.jpa;
 
+import com.boot.jpa.repository.EmployeeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootMybatisJpaApplicationTests {
 
+  @Autowired
+  private EmployeeRepository repository;
+
   @Test
   public void contextLoads() {
+  }
+
+  @Test
+  public void query(){
+    System.out.println(repository.findAll());
   }
 
 }
