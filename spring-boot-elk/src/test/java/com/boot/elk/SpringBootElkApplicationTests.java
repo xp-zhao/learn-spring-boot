526db927e55f4e7f8d0fb0de763a9ba8134f928c
xp-zhao <13688396271@163.com>
2019-07-09 15:09:57 +0800

    logback接入elk

--- spring-boot-elk/src/test/java/com/boot/elk/SpringBootElkApplicationTests.java
+++ spring-boot-elk/src/test/java/com/boot/elk/SpringBootElkApplicationTests.java
@@ -1,6 +1,7 @@
 package com.boot.elk;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +15,12 @@ public class SpringBootElkApplicationTests {
   public void contextLoads() {
   }
 
-  private Logger logger = Logger.getLogger(getClass());
+  private Logger logger = LogManager.getLogger(SpringBootElkApplicationTests.class);
 
   @Test
   public void test() throws Exception {
 
-    for(int i=0;i<100;i++) {
+    for(int i=0;i<10;i++) {
       logger.info("输出info  ");
       logger.debug("输出debug+skkkw嗡嗡嗡kw");
       logger.error("输出error  嗡嗡嗡我");
