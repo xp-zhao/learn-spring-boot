6ba91a3af909deaf25ea3204b9b97fd2aa2dddd5
xp-zhao <13688396271@163.com>
2019-11-12 12:01:23 +0800

    整合 Swagger2

--- spring-boot-power/src/main/java/com/boot/power/web/controller/UserController.java
+++ spring-boot-power/src/main/java/com/boot/power/web/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.boot.power.web.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @since 2019-11-12
  */
 @Controller
-@RequestMapping("/userinfoEntity")
-public class UserinfoController {
-
+@Api(tags = "用户管理相关接口")
+@RequestMapping("/user")
+public class UserController {
 }
 
