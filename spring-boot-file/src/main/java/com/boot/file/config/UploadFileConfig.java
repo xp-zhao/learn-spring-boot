4b5459f63cf5c6095bc2770a550c0f85118cc231
xp-zhao <13688396271@163.com>
2019-12-24 17:06:55 +0800

    文件上传配置 tomcat 虚拟目录

---
+++ spring-boot-file/src/main/java/com/boot/file/config/UploadFileConfig.java
@@ -0,0 +1,29 @@
+package com.boot.file.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * UploadFileConfig.java
+ * 文件上传配置
+ *
+ * @author: zhaoxiaoping
+ * @date: 2019/12/24
+ **/
+@Configuration
+public class UploadFileConfig extends WebMvcConfigurationSupport {
+
+	@Value("${uploadFile.location}")
+	private String uploadFileLocation;
+	@Value("${uploadFile.accessPath}")
+	private String accessPath;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(accessPath)
+				.addResourceLocations("file:" + uploadFileLocation + "/");
+		super.addResourceHandlers(registry);
+	}
+}
--- spring-boot-file/src/main/java/com/boot/file/controller/UploadController.java
+++ spring-boot-file/src/main/java/com/boot/file/controller/UploadController.java
@@ -1,5 +1,11 @@
 package com.boot.file.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,11 +19,25 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class UploadController {
 
-  @PostMapping("/upload")
-  public String upload(MultipartFile file) {
-    if (file == null || file.isEmpty()) {
-      return "文件为空";
-    }
-    return file.getOriginalFilename();
-  }
+	public static final Logger log = LoggerFactory.getLogger(UploadController.class);
+
+	@Value("${uploadFile.location}")
+	private String uploadFileLocation;
+	@Value("${uploadFile.accessPrefix}")
+	private String accessPrefix;
+
+	@PostMapping("/upload")
+	public String upload(MultipartFile file) throws IOException {
+		if (file == null || file.isEmpty()) {
+			return "文件为空";
+		}
+		String originName = file.getOriginalFilename();
+		// 文件后缀
+		String suffix = originName.substring(originName.lastIndexOf("."));
+		String fileName = UUID.randomUUID().toString().replace("-", "");
+		File saveFile = new File(uploadFileLocation, fileName + suffix);
+		file.transferTo(saveFile);
+		log.info("上传成功");
+		return accessPrefix + fileName + suffix;
+	}
 }
