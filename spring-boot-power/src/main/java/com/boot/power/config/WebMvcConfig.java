9d5ac6b9c9d8f879f283fc55dd1dcefa167d61ef
xp-zhao <13688396271@163.com>
2019-12-25 17:04:16 +0800

    拦截器示例

---
+++ spring-boot-power/src/main/java/com/boot/power/config/WebMvcConfig.java
@@ -0,0 +1,38 @@
+package com.boot.power.config;
+
+import com.boot.power.common.filter.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebMvcConfig.java
+ * 拦截器配置
+ *
+ * @author: zhaoxiaoping
+ * @date: 2019/12/25
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private MyInterceptor myInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowCredentials(true)
+				.maxAge(3600)
+				.allowedHeaders("*");
+	}
+
+}
--- spring-boot-power/src/main/java/com/boot/power/entity/UserGroupUserEntity.java
+++ spring-boot-power/src/main/java/com/boot/power/entity/UserGroupUserEntity.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ import lombok.ToString;
 @Data
 @Builder
 @ToString
+@AllArgsConstructor
 @NoArgsConstructor
 public class UserGroupUserEntity implements Serializable {
 
