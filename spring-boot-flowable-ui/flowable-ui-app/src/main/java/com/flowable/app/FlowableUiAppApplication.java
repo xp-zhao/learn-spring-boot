abb149daeba8d57da932c3f28609c6989fabedf0
xp-zhao <13688396271@163.com>
2021-03-10 18:06:35 +0800

    整合Flowable6.6.0前端页面

---
+++ spring-boot-flowable-ui/flowable-ui-app/src/main/java/com/flowable/app/FlowableUiAppApplication.java
@@ -0,0 +1,13 @@
+package com.flowable.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableUiAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowableUiAppApplication.class, args);
+  }
+
+}
---
+++ spring-boot-flowable-ui/flowable-ui-app/src/test/java/com/flowable/app/FlowableUiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowable.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableUiAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
