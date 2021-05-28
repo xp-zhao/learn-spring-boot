ee278d80a2f3c910e521ae0757f6fdc5b80c759e
xp-zhao <13688396271@163.com>
2021-05-28 22:32:28 +0800

    完善 oauth2.0 示例

--- spring-boot-oauth/oauth-client/src/main/java/com/boot/oauthclient/controller/LoginController.java
+++ spring-boot-oauth/oauth-client/src/main/java/com/boot/oauthclient/controller/LoginController.java
@@ -1,36 +1,60 @@
 package com.boot.oauthclient.controller;
 
 import cn.hutool.core.util.StrUtil;
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author zhaoxiaoping
  * @Description: 登录控制层
  * @Date 2021-5-28
  **/
-@RestController
+@Controller
 @RequestMapping("/client")
 @Slf4j
 public class LoginController {
 
-  /**
-   * 客户端登录, 重定向到同一登录服务进行登录
-   *
-   * @param response
-   * @throws IOException
-   */
-  @PostMapping("/login")
-  public void login(HttpServletResponse response) throws IOException {
-    String serverUrl = "http://localhost:7000/server/login";
-    String redirectUrl = "http://localhost:7001/client/index&scope=userinfo&state=hh";
-    String url = "{}?response_type={}&client_id={}&redirect_url={}";
-    response.sendRedirect(StrUtil.format(url, serverUrl, "code", "10001", redirectUrl));
-  }
-  
-  
+    /**
+     * 客户端登录, 重定向到同一登录服务进行登录
+     *
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/login")
+    public void login(HttpServletResponse response) throws IOException {
+        String serverUrl = "http://localhost:7000/server/authorize";
+        String redirectUrl = "http://localhost:7001/client/index&scope=userinfo&state=hh";
+        String url = "{}?responseType={}&clientId={}&redirectUrl={}";
+        log.info("重定向跳转");
+        response.sendRedirect(StrUtil.format(url, serverUrl, "code", "10001", redirectUrl));
+    }
+
+    @GetMapping("/index")
+    public String index(String code, HttpServletRequest request) {
+        RestTemplate restTemplate = new RestTemplateBuilder().build();
+        String accessToken = restTemplate.getForObject("http://localhost:7000/server/getTokenByCode?grant_type=authorization_code&code=xxx&" +
+                "redirect_uri=http://localhost:7001/client/index", String.class);
+        String username = restTemplate.getForObject("http://localhost:7000/server/getUserinfoByToken?" +
+                "access_token=yyy", String.class);
+        request.getSession().setAttribute("username",username);
+        return "index";
+    }
+
+    @PostMapping("getUserInfo")
+    @ResponseBody
+    public String getUserInfo(HttpServletRequest request) throws Exception {
+        Object username = request.getSession().getAttribute("username");
+        return "Tom 18811311416";
+    }
+
 }
--- spring-boot-oauth/oauth-server/src/main/java/com/boot/oauthserver/controller/LoginController.java
+++ spring-boot-oauth/oauth-server/src/main/java/com/boot/oauthserver/controller/LoginController.java
@@ -1,24 +1,64 @@
 package com.boot.oauthserver.controller;
 
-import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author zhaoxiaoping
  * @Description: 登录控制层
  * @Date 2021-5-28
  **/
-@RestController
+@Controller
 @RequestMapping("/server")
 @Slf4j
 public class LoginController {
 
-  @PostMapping("/login")
-  public String login(String username, String password, HttpServletResponse response) {
-    log.info("用户登录, username: {}", username);
-    return "success";
-  }
+    private Map<String, String> paramMap = new HashMap<>();
+
+    @GetMapping("/authorize")
+    public String login(String responseType, String clientId, String redirectUrl, String scope, String state) {
+        paramMap.put("responseType", responseType);
+        paramMap.put("clientId", clientId);
+        paramMap.put("redirectUrl", redirectUrl);
+        paramMap.put("scope", scope);
+        paramMap.put("state", state);
+        log.info("访问统一登录页面，进行用户登录, param: {}", paramMap);
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public void login(String username, String password, HttpServletResponse response) throws IOException {
+        log.info("统一登录服务，账号密码登录成功，重定向到客户端页面");
+        response.sendRedirect(paramMap.get("redirectUrl") + "?code=xxx&state=hehe");
+    }
+
+    @GetMapping("getTokenByCode")
+    @ResponseBody
+    public String getTokenByCode(String grantType, String code, String clientId, String redirectUri) throws IOException {
+        // 判断client_id、redirect_uri、code是否正确
+        log.info("通过校验码获取token");
+        // 返回token
+        return "{access_token:yyy," +
+                "token_type:bearer," +
+                "expires_in:600," +
+                "refresh_token:zzz}";
+    }
+
+    @GetMapping("getUserinfoByToken")
+    @ResponseBody
+    public String getUserinfoByToken(String token) throws IOException {
+        log.info("通过 token 获取用户信息");
+        // 判断token是否正确
+        return "{userGuid:j3jlk2jj32li43i," +
+                "username:Tom," +
+                "mobile:18811412324}";
+    }
 }
